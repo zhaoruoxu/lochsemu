eae25c6b1c70b45dd813077222b6279993ead635
zhaoruoxu <zhaoruoxu@gmail.com>
2013-01-05 12:15:37 +0800

    static analysis part

---
+++ Arietis/static/disassembler.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "disassembler.h"
+
+
+Disassembler::Disassembler()
+{
+
+}
+
+Disassembler::~Disassembler()
+{
+
+}
---
+++ Arietis/static/disassembler.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef __ARIETIS_STATIC_DISASSEMBLER_H__
+#define __ARIETIS_STATIC_DISASSEMBLER_H__
+
+#include "Arietis.h"
+
+class Disassembler {
+public:
+    Disassembler();
+    virtual ~Disassembler();
+};
+
+#endif // __ARIETIS_STATIC_DISASSEMBLER_H__
